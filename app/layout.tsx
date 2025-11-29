e4f6c467ce29b47c9162b7f2aa19ee0c7db281ca
Maximillien-kol <maximillien08@gmail.com>
2025-11-29 11:19:41 +0200

    fix: add strategy to adsense script to prevent data-nscript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1412133449814385"
           crossOrigin="anonymous"
+          strategy="afterInteractive"
         />
         <GoogleAnalytics />
       </body>
